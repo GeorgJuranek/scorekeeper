d577085c874a8eb08b8e8239c14fcefa3ab7be54
Georg Juranek <georg_juranek@hotmail.de>
2022-06-03 12:22:58 +0200

    State and Lifting Up State are done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 import Button from "./Button.js";
 import Player from "./Player.js";
 import Heading from "./Heading.js";
 import PlayerForm from "./PlayerForm.js";
 
 function App() {
+  const [players, setPlayers] = useState([]);
+  console.log(players);
+
+  function createPlayer(newPlayer) {
+    setPlayers([...players, newPlayer]);
+  }
+
   return (
     <div className="App">
       <Heading headtext="Scorekeeper Version A" />
@@ -12,7 +20,8 @@ function App() {
       <Player text="Dorian Jones" score={30} />
       <Button text="Reset scores" />
       <Button text="New game" />
-      <PlayerForm />
+
+      <PlayerForm onCreatePlayer={createPlayer} />
     </div>
   );
 }
--- src/PlayerForm.js
+++ src/PlayerForm.js
@@ -1,15 +1,24 @@
 import "./PlayerForm.css";
+import { nanoid } from "nanoid";
 
-export default function PlayerForm() {
+export default function PlayerForm({ onCreatePlayer }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const { addPlayer: addPlayerInput } = form.elements;
+    const newPlayer = { id: nanoid(), name: addPlayerInput.value, score: 0 };
+    onCreatePlayer(newPlayer);
+    form.reset();
+    addPlayerInput.focus();
+  }
+
   return (
-    <form>
-      <label htmlFor="newPlayer">
+    <form aria-labelledby="newPlayer" onSubmit={handleSubmit}>
+      <label htmlFor="addPlayer">
         Add Player
-        <input id="newPlayer" />
+        <input id="addPlayer" name="addPlayer" />
       </label>
-      <button onClick={addNewPlayerToArray()}>submit</button>
+      <button>submit</button>
     </form>
   );
 }
-
-function addNewPlayerToArray() {}
